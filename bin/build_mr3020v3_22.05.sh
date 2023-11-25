843cd71958834ef6c8a4dba0849c01c282d4f462
kassle <kassle@krybrig.org>
2023-11-25 10:37:40 +0700

    exclude opkg package

--- bin/build_mr3020v3_22.05.sh
+++ bin/build_mr3020v3_22.05.sh
@@ -1,5 +1,5 @@
 ## build default
-make image PROFILE="tplink_tl-mr3020-v3" PACKAGES="usb-modeswitch kmod-usb-net-cdc-ether kmod-usb-storage kmod-fs-ext4 blockd openvpn-mbedtls uhttpd uhttpd-mod-ubus libiwinfo-lua luci-base luci-app-firewall luci-mod-admin-full luci-theme-bootstrap luci-app-ksmbd luci-app-openvpn -ppp -ppp-mod-pppoe" FILES="files"
+make image PROFILE="tplink_tl-mr3020-v3" PACKAGES="usb-modeswitch kmod-usb-net-cdc-ether kmod-usb-storage kmod-fs-ext4 blockd openvpn-mbedtls uhttpd uhttpd-mod-ubus libiwinfo-lua luci-base luci-app-firewall luci-mod-admin-full luci-theme-bootstrap luci-app-ksmbd luci-app-openvpn -ppp -ppp-mod-pppoe -opkg" FILES="files"
 
 ## max - has issue can't save config
 # make image PROFILE="tplink_tl-mr3020-v3" PACKAGES="usb-modeswitch kmod-usb-net-cdc-ether kmod-usb-storage kmod-fs-ext4 blockd ethtool openvpn-mbedtls ip6tables-mod-nat kmod-ipt-nat6 uhttpd uhttpd-mod-ubus libiwinfo-lua luci-base luci-app-firewall luci-mod-admin-full luci-theme-bootstrap luci-app-ksmbd luci-app-https-dns-proxy luci-app-adblock-fast luci-app-openvpn gawk grep sed coreutils-sort -ppp -ppp-mod-pppoe -opkg" FILES="files"
