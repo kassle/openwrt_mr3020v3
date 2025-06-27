71fc52c0da79089903efc9253152d357f11b14c3
erdian <erdian@rayslan.com>
2025-06-27 22:37:52 +0700

    fix sysupgrade binary not moved to workdir

--- bin/tplink_mr3020v3.sh
+++ bin/tplink_mr3020v3.sh
@@ -22,12 +22,12 @@ fi
 cd $OPENWRT_WORKDIR
 make image PROFILE="tplink_tl-mr3020-v3" PACKAGES="usb-modeswitch kmod-usb-net-cdc-ether kmod-usb-storage kmod-fs-ext4 blockd uhttpd uhttpd-mod-ubus libiwinfo-lua luci-base luci-app-firewall luci-mod-admin-full luci-theme-bootstrap luci-app-ksmbd luci-proto-wireguard -ppp -ppp-mod-pppoe" FILES="../files"
 
-BIN1="bin/targets/ramips/mt76x8/openwrt-$OPENWRT_VERSION-ramips-mt76x8-tplink_tl-mr3020-v3-squashfs-sysupgrade.bin"
+BIN1="bin/targets/ramips/mt76x8/openwrt-${OPENWRT_VERSION}-ramips-mt76x8-tplink_tl-mr3020-v3-squashfs-sysupgrade.bin"
 if [ -f "$BIN1" ]; then
-    mv $BIN ../tplink-mr3020v3_$OPENWRT_VERSION_sysupgrade.bin
+    mv -v $BIN1 ../tplink-mr3020v3_${OPENWRT_VERSION}_sysupgrade.bin
 fi
 
-BIN2="bin/targets/ramips/mt76x8/openwrt-$OPENWRT_VERSION-ramips-mt76x8-tplink_tl-mr3020-v3-squashfs-tftp-recovery.bin"
+BIN2="bin/targets/ramips/mt76x8/openwrt-${OPENWRT_VERSION}-ramips-mt76x8-tplink_tl-mr3020-v3-squashfs-tftp-recovery.bin"
 if [ -f "$BIN2" ]; then
-    mv $BIN2 ../tplink-mr3020v3_$OPENWRT_VERSION_recovery.bin
+    mv -v $BIN2 ../tplink-mr3020v3_${OPENWRT_VERSION}_recovery.bin
 fi
